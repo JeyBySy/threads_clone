4f8da2fde89c754451d2af2e898280fcfc95db07
JeyBySy <njcocosa@gmail.com>
2025-05-23 00:22:01 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='bg-red-950'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt praesentium quae tempore quo ad impedit eligendi, recusandae dolorum nulla placeat exercitationem amet ipsum eius assumenda sit mollitia distinctio nesciunt eaque.</div>
+    </>
+  )
+}
+
+export default App
