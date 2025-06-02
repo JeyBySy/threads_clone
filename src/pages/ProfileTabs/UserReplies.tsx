336661349d585ab621ab19c42c049179a2b282f6
JeyBySy <njcocosa@gmail.com>
2025-06-02 16:59:20 +0800

    Add new pages

--- src/pages/ProfileTabs/UserReplies.tsx
+++ src/pages/ProfileTabs/UserReplies.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const UserReplies = () => {
     return (
         <div className='h-[58dvh] flex justify-center items-center text-neutral-500 text-[15px]'>No replies yet.</div>
--- src/pages/ProfileTabs/UserRepost.tsx
+++ src/pages/ProfileTabs/UserRepost.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const UserRepost = () => {
     return (
         <div className='h-[58dvh] flex justify-center items-center text-neutral-500 text-[15px]'>No reposts yet.</div>
